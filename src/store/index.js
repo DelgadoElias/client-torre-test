5e20c8c514c3177d74e314a0d91ed4bec313d2bd
Elias Lautaro Delgado <82496172+DelgadoElias@users.noreply.github.com>
2022-01-18 21:58:19 -0300

    Summary
    Improving ESLINT for code consistency

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 // Importaciones
-import { applyMiddleware, createStore } from 'redux'
-import reducer from './reducer'
-import thunk from 'redux-thunk'
+import {applyMiddleware, createStore} from 'redux';
+import reducer from './reducer';
+import thunk from 'redux-thunk';
 
 
 const store = createStore(reducer, applyMiddleware(thunk));
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,70 +1,88 @@
-import { FETCH_USER, FETCH_USER_EXPERIENCES, FETCH_USER_SKILLS, SET_TOAST } from "../actions";
+/* eslint valid-jsdoc: "error"*/
+/* eslint-env es6*/
 
+import {
+  FETCH_USER,
+  FETCH_USER_EXPERIENCES,
+  FETCH_USER_SKILLS,
+  SET_TOAST,
+} from '../actions';
 
 const initialState = {
-    person : {
-        publicId: ''
-    },
-    skills : [],
-    experiences : {
-        personal : [],
-        api : []
-    },
-    snack: ''
-}
+  person: {
+    publicId: '',
+  },
+  skills: [],
+  experiences: {
+    personal: [],
+    api: [],
+  },
+  snack: '',
+};
 
-export default function reducer(state = initialState, action){
-    switch(action.type){
+/**
+ * Adds two numbers together.
+ * @param {Object} state - State var. Store data about person & the app.
+ * @param {Object} action - Contains type and a payload.
+ * @param {Object} action.type - Type to switch actions in reducer.
+ * @param {Object} action.payload - Contains data to be used in the state .
+ * @returns {object} State equal or modified.
+ */
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_USER:
+      const person = action.payload;
+      return {
+        ...state,
+        person,
+      };
 
-        case FETCH_USER:
-            console.log('Llegué', action.payload);
-            let person = action.payload
-            return {
-                ...state,
-                person
-            };
-        
-        case FETCH_USER_SKILLS:
-            const master = action.payload.filter((x) => x.proficiency === "master")
-            const expert = action.payload.filter((x) => x.proficiency === "expert")
-            const proficient = action.payload.filter((x) => x.proficiency === "proficient")
-            const novice = action.payload.filter((x) => x.proficiency === "novice")
-            
-            master.length === 0 ? master.push({name: 'None'}) : console.log('OK');
-            expert.length === 0 ? expert.push({name: 'None'}) : console.log('OK');
-            proficient.length === 0 ? proficient.push({name: 'None'}) : console.log('OK');
-            novice.length === 0 ? novice.push({name: 'None'}) : console.log('OK');
+    case FETCH_USER_SKILLS:
+      const master = action.payload.filter((x) => x.proficiency === 'master');
+      const expert = action.payload.filter((x) => x.proficiency === 'expert');
+      const proficient = action.payload.filter(
+          (x) => x.proficiency === 'proficient');
+      const novice = action.payload.filter((x) => x.proficiency === 'novice');
+      if (master.length === 0) {
+        master.push({name: 'None'});
+      }
+      if (expert.length === 0) {
+        expert.push({name: 'None'});
+      }
+      if (proficient.length === 0) {
+        proficient.push({name: 'None'});
+      }
+      if (novice.length === 0) {
+        proficient.push({name: 'None'});
+      }
 
+      return {
+        ...state,
+        skills: {
+          master,
+          expert,
+          proficient,
+          novice,
+        },
+      };
 
-            return {
-                ...state,
-                skills : {
-                    master,
-                    expert,
-                    proficient,
-                    novice
-                }
-            };
+    case FETCH_USER_EXPERIENCES:
 
-            case FETCH_USER_EXPERIENCES:
-                console.log('PERSONAL', action.payload[0]);
-                console.log('API', action.payload[1]);
+      return {
+        ...state,
+        experiences: {
+          personal: action.payload[0],
+          api: action.payload[1],
+        },
+      };
 
-            return {
-                ...state,
-                experiences: {
-                    personal : action.payload[0],
-                    api : action.payload[1]
-                }
-            }
+    case SET_TOAST:
+      return {
+        ...state,
+        snack: action.payload,
+      };
 
-            case SET_TOAST: return{
-                ...state,
-                snack : action.payload
-            }                
-
-        default: 
-        console.log('Estoy entrando acá');
-            return state;
-    }
+    default:
+      return state;
+  }
 }
